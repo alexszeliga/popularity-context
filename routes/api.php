934d6757714976d015561440e3e9845475e155c0
Alex Szeliga <alexszeliga@gmail.com>
2024-10-12 15:02:26 +0000

    refactor to use middleware group for basic auth

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Resources\User\LoginResource;
 
-Route::get('/login', function() {
-    return new LoginResource(Auth::user());
-})->middleware('auth.basic');
+Route::middleware('auth.basic')->group(function(){
+    Route::get('/login', function() { return new LoginResource(Auth::user()); });
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
