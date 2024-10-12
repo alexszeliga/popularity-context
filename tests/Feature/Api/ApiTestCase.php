860f0d476d7a4cefd503d48310601b7a94b7d5c6
Alex Szeliga <alexszeliga@gmail.com>
2024-10-12 13:56:39 +0000

    test improvements: add and refactor to use ApiTestCase with setUp to request application/json, refactor tests for simpler routes

---
+++ tests/Feature/Api/ApiTestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+
+abstract class ApiTestCase extends TestCase
+{
+    public function setUp() : void 
+    {
+        parent::setUp();
+        $this->withHeaders([
+            'Accept' => 'application/json',
+        ]);
+    }
+}
--- tests/Feature/Api/AuthTest.php
+++ tests/Feature/Api/AuthTest.php
@@ -2,23 +2,32 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
 
-class AuthTest extends TestCase
+class AuthTest extends ApiTestCase
 {
-    /**
-     * A basic feature test example.
-     */
+    protected $defaultUser;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+        $this->defaultUser = User::firstWhere('email', 'alexszeliga@gmail.com');
+    }
+
     public function testDefaultSuperAdminCanLogin(): void
     {
         $basicAuthB64 = base64_encode('alexszeliga@gmail.com:password');
         $response = $this->withHeaders(['Authorization' => "Basic $basicAuthB64"])
                          ->get('/api/login');
-
         $response->assertStatus(200);
+        $response->assertSeeText('token');
+        $this->defaultUser->tokens->each(function($t) use ($response) {
+            $response->assertSeeText( ( $t->plainTextToken ) );
+        });
     }
+
     public function testDefaultSuperAdminCannotLoginWithIncorrectPassword(): void
     {
         $basicAuthB64 = base64_encode('alexszeliga@gmail.com:wrong!');
@@ -26,5 +35,13 @@ class AuthTest extends TestCase
                          ->get('/api/login');
 
         $response->assertStatus(401);
+        $response->assertSee('Invalid credentials.');
+    }
+
+    public function testLoginRequiresBasicAuth() : void
+    {
+        $response = $this->get('/api/login');
+        $response->assertStatus(401);
+        $response->assertSee('Invalid credentials.');
     }
 }
