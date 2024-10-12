48d2062e2fb85134c761ad0cb1aec345461f0fca
Alex Szeliga <alexszeliga@gmail.com>
2024-10-12 13:55:13 +0000

    update api to remove auth controller

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
-Route::controller(AuthController::class)->group(function() {
-    Route::get('/login', 'login');
-});
-
+Route::get('/login', function(){
+    return response()->json([
+        'data' => 
+            ['token' => Auth::user()->createToken('api-login')->plainTextToken],
+    ]);
+})->middleware('auth.basic');
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
