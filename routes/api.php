6cd5ab5c909f5ae7a5ee20ff9fd5d6d271ce7fc1
Alex Szeliga <alexszeliga@gmail.com>
2024-10-25 01:06:10 +0000

    implement verify route

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,11 @@ Route::middleware('auth.basic')->group(function() {
     Route::get( '/login', fn() => new LoginResource(Auth::user()) );
 });
 
-Route::middleware('auth:sanctum')->group(function(){
+Route::get('/user/send-verification-link', function (Request $r) {
+    $r->user()->sendEmailVerificationNotification();
+    return ['message'=>'Email verification email sent'];
+})->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function() {
     Route::get('/user', [UserController::class, 'show'])->name('user.profile');
 });
