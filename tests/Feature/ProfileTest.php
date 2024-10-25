b7afda758f3d0b55a699c2bf2d0068351e2b95a7
Alex Szeliga <alexszeliga@gmail.com>
2024-10-25 01:16:29 +0000

    clean up imports

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -3,14 +3,11 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Volt\Volt;
 use Tests\TestCase;
 
 class ProfileTest extends TestCase
 {
-    use RefreshDatabase;
-
     public function test_profile_page_is_displayed(): void
     {
         $user = User::factory()->create();
